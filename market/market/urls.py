490bcd308bac147e03cc31df16798a2ae03d0af8
Malanichev Aleksey <malanichevab@gmail.com>
2023-05-25 17:15:29 +0300

    add models for fun and exercises

--- market/market/urls.py
+++ market/market/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
+# У вас уже есть это условие
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),) 
